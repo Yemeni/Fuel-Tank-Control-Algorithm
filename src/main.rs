f2fc3db95320031955d6572f299bce62c4a84d74
Yousef Al-Hadhrami <yhadhr@gmail.com>
2024-07-21 01:15:36 +0200

    cleaning before starting

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,17 @@
 mod control_algorithm;
 
-use std::collections::HashMap;
-use std::ffi::c_char;
-use std::hash::Hash;
-
 fn main(){
 
 }
 
-fn foo(left: &str, right: &str) -> bool{
 
-    false
-}
 
-fn count(s: &str) -> HashMap<char, u8> {
-    let mut counter: HashMap<char, u8> = HashMap::new();
-
-    for c in s.chars() {
-        let count = counter.entry(c).or_insert(0);
-        *count += 1;
-    }
-
-    counter
-}
-
-#[cfg(test)]
-mod tests{
-    use crate::foo;
-
-    #[test]
-    fn foo_ear_hear(){
-        assert!(!foo("ear", "hear"))
-    }
-}
+// #[cfg(test)]
+// mod tests{
+//     use crate::foo;
+//
+//     #[test]
+//     fn foo_ear_hear(){
+//         assert!(!foo("ear", "hear"))
+//     }
+// }

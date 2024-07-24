4130c04924a8c6a95c5164bb62ffaa821493e615
Yousef Al-Hadhrami <yhadhr@gmail.com>
2024-07-24 20:10:54 +0200

    Simplifying the code

--- src/control_algorithm.rs
+++ src/control_algorithm.rs
@@ -1,21 +1,23 @@
-//! Implementation of the control algorithm. IMPLEMENT YOUR CONTROL ALGORITHM HERE
-
 use crate::{ControlAlgorithm, Tank, TankSystem};
 
 #[derive(Debug)]
-struct SimpleControlAlgorithm{
+pub struct SimpleControlAlgorithm {
     // Here you can add variables to your control algorithm if you need any
     // This may be helpful if your algorithm needs to remember anything in between calls to `control`
 }
 
 impl SimpleControlAlgorithm {
     // Here you can add additional functions to your control algorithm if you need any
+    pub fn new() -> Self {
+        Self {
+            // Initialize any variables if needed
+        }
+    }
 }
-
 
 impl ControlAlgorithm for SimpleControlAlgorithm {
     // THIS SHOULD BE IMPLEMENTED BY YOU
-    // 
+    //
     // Your algorithm should prevent the tank system from overflowing
     //
     // This function provides you with the `system` parameter
@@ -31,6 +33,19 @@ impl ControlAlgorithm for SimpleControlAlgorithm {
     // This functions needs to return either `Tank::Left` or `Tank::Right` specifying
     // the desired position of the valve
     fn control(&mut self, system: Box<&dyn TankSystem>) -> Tank {
-        todo!("Implement your control algorithm here")
+        let left_level = system.level_left();
+        let right_level = system.level_right();
+        let left_capacity = system.fuel_capacity_left();
+        let right_capacity = system.fuel_capacity_right();
+
+        if left_level > left_capacity * 0.9 {
+            return Tank::Right;
+        } else if right_level > right_capacity * 0.9 {
+            return Tank::Left;
+        } else if left_level < right_level {
+            return Tank::Left;
+        } else {
+            return Tank::Right;
+        }
     }
 }
--- tests/control_algorithm.rs
+++ tests/control_algorithm.rs
@@ -1,8 +1,33 @@
-use cucumber::World;
-use fuel_control::simulatorca::TankSimulation;
+use cucumber::{given, then, when, World};
+use fuel_control::simulator::TankSimulation;
+use fuel_control::Tank;
 
 // Add your "given", "when", "then" functions here
 
+#[given(expr = "the left tank is at {float} liter")]
+fn set_left_tank(world: &mut TankWorld, level: f32) {
+    world.tank.level_left = level;
+}
+
+#[given(expr = "the right tank is at {float} liter")]
+fn set_right_tank(world: &mut TankWorld, level: f32) {
+    world.tank.level_right = level;
+}
+
+#[when(expr = "{int} seconds pass")]
+fn pass_time(world: &mut TankWorld, seconds: u64) {
+    world.tank.advance(std::time::Duration::from_secs(seconds));
+}
+
+#[then(expr = "the left tank should not overflow")]
+fn check_left_overflow(world: &mut TankWorld) {
+    assert!(!world.tank.overflowed_left(), "Left tank overflowed");
+}
+
+#[then(expr = "the right tank should not overflow")]
+fn check_right_overflow(world: &mut TankWorld) {
+    assert!(!world.tank.overflowed_right(), "Right tank overflowed");
+}
 
 // NO CHANGES NEED TO BE DONE TO THIS WORLD OBJECT (but you can if you want)
 #[derive(Debug, Default, World)]
